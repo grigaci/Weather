6dfb3932c46aea84d7f80b11da4f9aebec93088a
Bogdan Iusco <bogdan.iusco@gmail.com>
2018-12-20 20:32:56 +0200

    Add draft project.

---
+++ Weather/GUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Bogdan Iusco on 12/20/18.
+//  Copyright © 2018 iGama Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
